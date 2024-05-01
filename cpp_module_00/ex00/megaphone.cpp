6048f8a6e82042213919b9ced5c61d6603b1b3fe
Zaid Kotbi <zkotbi@e1r11p2.1337.ma>
2024-05-01 12:44:48 +0100

    ex00

--- cpp_module_00/ex00/megaphone.cpp
+++ cpp_module_00/ex00/megaphone.cpp
@@ -6,13 +6,34 @@
 /*   By: zkotbi <zkotbi@1337.ma>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/01 10:34:53 by zkotbi            #+#    #+#             */
-/*   Updated: 2024/05/01 10:36:04 by zkotbi           ###   ########.fr       */
+/*   Updated: 2024/05/01 12:42:27 by zkotbi           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 
-int main()
+void print_upp(char **argv)
 {
-	return (0);
+	int j, i = 1;
+
+	while (argv[i] != NULL)
+	{
+		j = 0;
+		while (argv[i][j] != 0)
+		{
+			if (argv[i][j] >= 'a' && argv[i][j] <= 'z')
+				argv[i][j] -= 32;
+			std::cout << argv[i][j];
+			j++;
+		}
+		i++;
+	}
+}
+
+int main(int argc, char **argv)
+{
+	if (argc == 1)
+		return (std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n" , 1);
+	print_upp(argv);
+	std::cout << "\n";
 }
