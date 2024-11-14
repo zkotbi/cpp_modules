c1d7251db2688ab55b379c63bddd07fc8464abe7
Zaid Kotbi <zkotbi@e3r11p3.1337.ma>
2024-11-14 22:39:42 +0100

    add somr tests to Cpp08/ex00/

--- Cpp08/ex00/main.cpp
+++ Cpp08/ex00/main.cpp
@@ -5,46 +5,23 @@
 
 int main()
 {
-	{
-		std::vector<int> vec(8);
-		std::cout << "the container's elements: " << std::endl;
-		for (size_t i = 0; i < 8; i++) {
-			vec[i] = rand() % 10;
-			std::cout << vec[i] << "  ";
-		}
-		 std::cout << std::endl;
-		try 
-		{
-			easyfind(vec, 5);
-			std::cout << "integer fonded" << std::endl;
-		}
-		catch (std::runtime_error& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
-		try 
-		{
-			easyfind(vec, 100);
-			std::cout << "integer fonded" << std::endl;
-		}
-		catch (std::runtime_error& e)
-		{
-			std::cout << e.what() << std::endl;
-		}
+	srand(time(NULL));
+	std::vector<int>::iterator  it;
+	std::vector<int> vec(8);
+	std::cout << "the container's(vector) elements: " << std::endl;
+	for (size_t i = 0; i < 8; i++) {
+		vec[i] = rand() % 10;
+		std::cout << vec[i] << "  ";
 	}
+	std::cout << std::endl;
+	int integer = rand() % 8;
+	try 
 	{
-		// std::vector<std::string> vec(8);
-		// for (size_t i = 0; vec.size(); i++) {
-		// 	vec[i] = "HELLO";
-		// }
-		// try 
-		// {
-		// 	easyfind(vec, 100);
-		// 	std::cout << "integer fonded" << std::endl;
-		// }
-		// catch (std::runtime_error& e)
-		// {
-		// 	std::cout << e.what() << std::endl;
-		// }
+		it = easyfind(vec, integer);
+		std::cout << integer << " is founded" << std::endl;
+	}
+	catch (std::runtime_error& e)
+	{
+		std::cout << e.what() << ": " << integer << std::endl;
 	}
 }
